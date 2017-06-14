4d909b6433b5eb6ad26eed145c9dbe1cde1153f9
RTimal <rtimal@gmail.com>
2017-06-13 22:19:45 -0400

    hooked up auto seaerch

--- WikipediaImageSearch/app/src/main/java/com/rajievtimal/wikipediaimagesearch/search/MainActivity.java
+++ WikipediaImageSearch/app/src/main/java/com/rajievtimal/wikipediaimagesearch/search/MainActivity.java
@@ -1,14 +1,16 @@
 package com.rajievtimal.wikipediaimagesearch.search;
 
+import android.animation.LayoutTransition;
 import android.databinding.BindingAdapter;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.rajievtimal.wikipediaimagesearch.R;
@@ -17,6 +19,7 @@ import com.rajievtimal.wikipediaimagesearch.base.ServiceCallback;
 import com.rajievtimal.wikipediaimagesearch.entities.Page;
 
 import java.lang.ref.WeakReference;
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Random;
 
@@ -30,6 +33,8 @@ public class MainActivity extends BaseActivity implements ImageResponder {
     private PageImagesAdapter mPageImagesAdapter;
     private String mSearchTerm = "";
     private char[] mAlphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();    //TODO: Use this
+    private SearchView mSearchView;
+    private EditText mSearchViewEditText;
 
     @BindingAdapter({"bind:imageUrl"})
     public static void loadImage(final ImageView view, final String url) {
@@ -51,15 +56,24 @@ public class MainActivity extends BaseActivity implements ImageResponder {
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        setupSearchView();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        //Show random images based on letter if there is no search term
         if (mSearchTerm.length() == 0) {
             searchForRandomImages();
         }
+    }
+
+    void searchForImagesWithTerm(final String searchTerm) {
+        SearchService.getInstance().searchForImagesWithTerm(searchTerm, new ServiceCallback<List<Page>>() {
+            @Override
+            public void finishedLoading(List<Page> pages, String error) {
+                setImages(pages, searchTerm, false);
+            }
+        });
     }
 
     void searchForRandomImages() {
@@ -73,16 +87,8 @@ public class MainActivity extends BaseActivity implements ImageResponder {
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.search, menu);
-
-        return true;
-    }
-
     void setImages(List<Page> pages, String searchTerm, Boolean random) {
-        if (pages != null || pages.size() > 0) {
+        if (pages != null && pages.size() > 0) {
             mPageImagesAdapter.clearItems();
             mPageImagesAdapter.addItems(pages);
             if (random) {
@@ -96,6 +102,60 @@ public class MainActivity extends BaseActivity implements ImageResponder {
             String message = "There are no results for" + '"' + mSearchTerm + '"' + ". Please try again.";
             mTextMessage.setText(message);
         }
+    }
+
+    private void setupSearchView() {
+        mSearchView = (SearchView) findViewById(R.id.search_view);
+        mSearchView.setFocusable(true);
+        mSearchView.setIconified(false);
+        mSearchView.setIconifiedByDefault(false);
+        mSearchView.requestFocusFromTouch();
+        mSearchView.setMaxWidth(100000000);
+        mSearchView.setQueryHint("Search for images");
+        final LinearLayout searchBar = (LinearLayout) mSearchView.findViewById(R.id.search_bar);
+        searchBar.setLayoutTransition(new LayoutTransition());
+        mSearchView.setActivated(true);
+        mSearchViewEditText = (EditText) mSearchView.findViewById(android.support.v7.appcompat.R.id.search_src_text);
+//        changeSearchViewTextColor(searchView);
+
+        try {
+            Field mCursorDrawableRes = TextView.class.getDeclaredField("mCursorDrawableRes");
+            mCursorDrawableRes.setAccessible(true);
+//            mCursorDrawableRes.set(searchViewEditText, R.drawable.white_cursor); // 0: @null (automatic), or any drawable of yours.
+//            mSearchView.setHintTextColor(getResources().getColor(R.color.linkedingray));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+//
+//        if (!TextUtils.isEmpty(author)) {
+//            searchViewEditText.setText(author);
+//            searchViewEditText.setSelection(author.length());
+//            currentTerm = "inauthor:" + "\"" + author + "\"";
+//            searchWithTerm();
+//            searchView.clearFocus();
+//        } else if (!TextUtils.isEmpty(isbn)) {
+//            searchViewEditText.setText(isbn);
+//            searchViewEditText.setSelection(isbn.length());
+//            currentTerm = isbn;
+//            searchWithTerm();
+//            searchView.clearFocus();
+//        }
+
+        mSearchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                mSearchTerm = query;
+                searchForImagesWithTerm(mSearchTerm);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                mSearchTerm = newText;
+                searchForImagesWithTerm(newText);
+                return true;
+            }
+        });
     }
 
     @Override
